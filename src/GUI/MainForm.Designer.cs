ac2938585eca51fd92fc24b5d2bd7c32c2401877
Antonia-1030 <63414480+Antonia-1030@users.noreply.github.com>
2022-03-24 20:55:40 +0200

    Add files via upload

---
+++ src/GUI/MainForm.Designer.cs
@@ -0,0 +1,327 @@
+﻿namespace Draw
+{
+	partial class MainForm
+	{
+		/// <summary>
+		/// Designer variable used to keep track of non-visual components.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+		
+		/// <summary>
+		/// Disposes resources used by the form.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing) {
+				if (components != null) {
+					components.Dispose();
+				}
+			}
+			base.Dispose(disposing);
+		}
+		
+		/// <summary>
+		/// This method is required for Windows Forms designer support.
+		/// Do not change the method contents inside the source code editor. The Forms designer might
+		/// not be able to load this method if it was changed manually.
+		/// </summary>
+		private void InitializeComponent()
+		{
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.mainMenu = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openFromToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.editToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.deleteToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.imageToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.statusBar = new System.Windows.Forms.StatusStrip();
+            this.currentStatusLabel = new System.Windows.Forms.ToolStripStatusLabel();
+            this.speedMenu = new System.Windows.Forms.ToolStrip();
+            this.toolStripButton2 = new System.Windows.Forms.ToolStripButton();
+            this.pickUpSpeedButton = new System.Windows.Forms.ToolStripButton();
+            this.drawRectangleSpeedButton = new System.Windows.Forms.ToolStripButton();
+            this.toolStripButton1 = new System.Windows.Forms.ToolStripButton();
+            this.toolStripButton3 = new System.Windows.Forms.ToolStripButton();
+            this.toolStripButton5 = new System.Windows.Forms.ToolStripButton();
+            this.toolStripButton4 = new System.Windows.Forms.ToolStripButton();
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.toolStripButton6 = new System.Windows.Forms.ToolStripButton();
+            this.colorDialog2 = new System.Windows.Forms.ColorDialog();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.viewPort = new Draw.DoubleBufferedPanel();
+            this.mainMenu.SuspendLayout();
+            this.statusBar.SuspendLayout();
+            this.speedMenu.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // mainMenu
+            // 
+            this.mainMenu.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.mainMenu.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.editToolStripMenuItem,
+            this.imageToolStripMenuItem,
+            this.helpToolStripMenuItem});
+            this.mainMenu.Location = new System.Drawing.Point(0, 0);
+            this.mainMenu.Name = "mainMenu";
+            this.mainMenu.Size = new System.Drawing.Size(924, 28);
+            this.mainMenu.TabIndex = 1;
+            this.mainMenu.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.exitToolStripMenuItem,
+            this.saveToolStripMenuItem,
+            this.openFromToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(46, 24);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.ExitToolStripMenuItemClick);
+            // 
+            // saveToolStripMenuItem
+            // 
+            this.saveToolStripMenuItem.Name = "saveToolStripMenuItem";
+            this.saveToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.saveToolStripMenuItem.Text = "Save";
+            this.saveToolStripMenuItem.Click += new System.EventHandler(this.saveToolStripMenuItem_Click);
+            // 
+            // openFromToolStripMenuItem
+            // 
+            this.openFromToolStripMenuItem.Name = "openFromToolStripMenuItem";
+            this.openFromToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.openFromToolStripMenuItem.Text = "Open from...";
+            this.openFromToolStripMenuItem.Click += new System.EventHandler(this.openFromToolStripMenuItem_Click);
+            // 
+            // editToolStripMenuItem
+            // 
+            this.editToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.deleteToolStripMenuItem});
+            this.editToolStripMenuItem.Name = "editToolStripMenuItem";
+            this.editToolStripMenuItem.Size = new System.Drawing.Size(49, 24);
+            this.editToolStripMenuItem.Text = "Edit";
+            // 
+            // deleteToolStripMenuItem
+            // 
+            this.deleteToolStripMenuItem.Name = "deleteToolStripMenuItem";
+            this.deleteToolStripMenuItem.Size = new System.Drawing.Size(136, 26);
+            this.deleteToolStripMenuItem.Text = "Delete";
+            // 
+            // imageToolStripMenuItem
+            // 
+            this.imageToolStripMenuItem.Name = "imageToolStripMenuItem";
+            this.imageToolStripMenuItem.Size = new System.Drawing.Size(65, 24);
+            this.imageToolStripMenuItem.Text = "Image";
+            // 
+            // helpToolStripMenuItem
+            // 
+            this.helpToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.aboutToolStripMenuItem});
+            this.helpToolStripMenuItem.Name = "helpToolStripMenuItem";
+            this.helpToolStripMenuItem.Size = new System.Drawing.Size(55, 24);
+            this.helpToolStripMenuItem.Text = "Help";
+            // 
+            // aboutToolStripMenuItem
+            // 
+            this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(142, 26);
+            this.aboutToolStripMenuItem.Text = "About...";
+            // 
+            // statusBar
+            // 
+            this.statusBar.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.statusBar.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.currentStatusLabel});
+            this.statusBar.Location = new System.Drawing.Point(0, 499);
+            this.statusBar.Name = "statusBar";
+            this.statusBar.Padding = new System.Windows.Forms.Padding(1, 0, 19, 0);
+            this.statusBar.Size = new System.Drawing.Size(924, 22);
+            this.statusBar.TabIndex = 2;
+            this.statusBar.Text = "statusStrip1";
+            // 
+            // currentStatusLabel
+            // 
+            this.currentStatusLabel.Name = "currentStatusLabel";
+            this.currentStatusLabel.Size = new System.Drawing.Size(0, 16);
+            // 
+            // speedMenu
+            // 
+            this.speedMenu.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.speedMenu.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripButton2,
+            this.pickUpSpeedButton,
+            this.drawRectangleSpeedButton,
+            this.toolStripButton1,
+            this.toolStripButton3,
+            this.toolStripButton5,
+            this.toolStripButton4,
+            this.toolStripButton6});
+            this.speedMenu.Location = new System.Drawing.Point(0, 28);
+            this.speedMenu.Name = "speedMenu";
+            this.speedMenu.Size = new System.Drawing.Size(924, 27);
+            this.speedMenu.TabIndex = 3;
+            this.speedMenu.Text = "toolStrip1";
+            // 
+            // toolStripButton2
+            // 
+            this.toolStripButton2.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.toolStripButton2.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton2.Image")));
+            this.toolStripButton2.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton2.Name = "toolStripButton2";
+            this.toolStripButton2.Size = new System.Drawing.Size(29, 24);
+            this.toolStripButton2.Text = "toolStripButton2";
+            this.toolStripButton2.Click += new System.EventHandler(this.SaveButton2);
+            // 
+            // pickUpSpeedButton
+            // 
+            this.pickUpSpeedButton.CheckOnClick = true;
+            this.pickUpSpeedButton.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.pickUpSpeedButton.Image = ((System.Drawing.Image)(resources.GetObject("pickUpSpeedButton.Image")));
+            this.pickUpSpeedButton.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.pickUpSpeedButton.Name = "pickUpSpeedButton";
+            this.pickUpSpeedButton.Size = new System.Drawing.Size(29, 24);
+            this.pickUpSpeedButton.Text = "toolStripButton1";
+            // 
+            // drawRectangleSpeedButton
+            // 
+            this.drawRectangleSpeedButton.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.drawRectangleSpeedButton.Image = ((System.Drawing.Image)(resources.GetObject("drawRectangleSpeedButton.Image")));
+            this.drawRectangleSpeedButton.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.drawRectangleSpeedButton.Name = "drawRectangleSpeedButton";
+            this.drawRectangleSpeedButton.Size = new System.Drawing.Size(29, 24);
+            this.drawRectangleSpeedButton.Text = "DrawRectangleButton";
+            this.drawRectangleSpeedButton.Click += new System.EventHandler(this.DrawRectangleSpeedButtonClick);
+            // 
+            // toolStripButton1
+            // 
+            this.toolStripButton1.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.toolStripButton1.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton1.Image")));
+            this.toolStripButton1.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton1.Name = "toolStripButton1";
+            this.toolStripButton1.Size = new System.Drawing.Size(29, 24);
+            this.toolStripButton1.Text = "toolStripButton1";
+            this.toolStripButton1.Click += new System.EventHandler(this.DrawEllipseButtonClick);
+            // 
+            // toolStripButton3
+            // 
+            this.toolStripButton3.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.toolStripButton3.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton3.Image")));
+            this.toolStripButton3.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton3.Name = "toolStripButton3";
+            this.toolStripButton3.Size = new System.Drawing.Size(29, 24);
+            this.toolStripButton3.Text = "toolStripButton3";
+            this.toolStripButton3.Click += new System.EventHandler(this.DrawLineButtonClick);
+            // 
+            // toolStripButton5
+            // 
+            this.toolStripButton5.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.toolStripButton5.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton5.Image")));
+            this.toolStripButton5.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton5.Name = "toolStripButton5";
+            this.toolStripButton5.Size = new System.Drawing.Size(29, 24);
+            this.toolStripButton5.Text = "toolStripButton5";
+            // 
+            // toolStripButton4
+            // 
+            this.toolStripButton4.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.toolStripButton4.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton4.Image")));
+            this.toolStripButton4.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton4.Name = "toolStripButton4";
+            this.toolStripButton4.Size = new System.Drawing.Size(29, 24);
+            this.toolStripButton4.Text = "toolStripButton4";
+            this.toolStripButton4.Click += new System.EventHandler(this.ColorToolButtonClick);
+            // 
+            // toolStripButton6
+            // 
+            this.toolStripButton6.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.toolStripButton6.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton6.Image")));
+            this.toolStripButton6.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton6.Name = "toolStripButton6";
+            this.toolStripButton6.Size = new System.Drawing.Size(29, 24);
+            this.toolStripButton6.Text = "toolStripButton6";
+            this.toolStripButton6.Click += new System.EventHandler(this.toolStripButton6_Click_1);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // viewPort
+            // 
+            this.viewPort.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.viewPort.Location = new System.Drawing.Point(0, 55);
+            this.viewPort.Margin = new System.Windows.Forms.Padding(5);
+            this.viewPort.Name = "viewPort";
+            this.viewPort.Size = new System.Drawing.Size(924, 444);
+            this.viewPort.TabIndex = 4;
+            this.viewPort.Load += new System.EventHandler(this.viewPort_Load);
+            this.viewPort.Paint += new System.Windows.Forms.PaintEventHandler(this.ViewPortPaint);
+            this.viewPort.MouseDown += new System.Windows.Forms.MouseEventHandler(this.ViewPortMouseDown);
+            this.viewPort.MouseMove += new System.Windows.Forms.MouseEventHandler(this.ViewPortMouseMove);
+            this.viewPort.MouseUp += new System.Windows.Forms.MouseEventHandler(this.ViewPortMouseUp);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(924, 521);
+            this.Controls.Add(this.viewPort);
+            this.Controls.Add(this.speedMenu);
+            this.Controls.Add(this.statusBar);
+            this.Controls.Add(this.mainMenu);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.mainMenu;
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "MainForm";
+            this.Text = "Draw";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.mainMenu.ResumeLayout(false);
+            this.mainMenu.PerformLayout();
+            this.statusBar.ResumeLayout(false);
+            this.statusBar.PerformLayout();
+            this.speedMenu.ResumeLayout(false);
+            this.speedMenu.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+		}
+		
+		private System.Windows.Forms.ToolStripStatusLabel currentStatusLabel;
+		private Draw.DoubleBufferedPanel viewPort;
+		private System.Windows.Forms.ToolStripButton pickUpSpeedButton;
+		private System.Windows.Forms.ToolStripButton drawRectangleSpeedButton;
+		private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
+		private System.Windows.Forms.ToolStripMenuItem helpToolStripMenuItem;
+		private System.Windows.Forms.ToolStripMenuItem imageToolStripMenuItem;
+		private System.Windows.Forms.ToolStripMenuItem editToolStripMenuItem;
+		private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+		private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+		private System.Windows.Forms.ToolStrip speedMenu;
+		private System.Windows.Forms.StatusStrip statusBar;
+		private System.Windows.Forms.MenuStrip mainMenu;
+        private System.Windows.Forms.ToolStripButton toolStripButton1;
+        private System.Windows.Forms.ToolStripButton toolStripButton2;
+        private System.Windows.Forms.ToolStripButton toolStripButton3;
+        private System.Windows.Forms.ToolStripButton toolStripButton4;
+        private System.Windows.Forms.ToolStripButton toolStripButton5;
+        private System.Windows.Forms.ColorDialog colorDialog1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.ToolStripMenuItem saveToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem deleteToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openFromToolStripMenuItem;
+        private System.Windows.Forms.ToolStripButton toolStripButton6;
+        private System.Windows.Forms.ColorDialog colorDialog2;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+    }
+}
---
+++ src/GUI/MainForm.cs
@@ -0,0 +1,213 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Windows.Forms;
+
+namespace Draw
+{
+	/// <summary>
+	/// Върху главната форма е поставен потребителски контрол,
+	/// в който се осъществява визуализацията
+	/// </summary>
+	public partial class MainForm : Form
+	{
+		/// <summary>
+		/// Агрегирания диалогов процесор във формата улеснява манипулацията на модела.
+		/// </summary>
+		private DialogProcessor dialogProcessor = new DialogProcessor();
+		
+		public MainForm()
+		{
+			//
+			// The InitializeComponent() call is required for Windows Forms designer support.
+			//
+			InitializeComponent();
+			
+			//
+			// TODO: Add constructor code after the InitializeComponent() call.
+			//
+		}
+
+		/// <summary>
+		/// Изход от програмата. Затваря главната форма, а с това и програмата.
+		/// </summary>
+		void ExitToolStripMenuItemClick(object sender, EventArgs e)
+		{
+			SaveFileDialog saveFileDialog1 = new SaveFileDialog();
+			saveFileDialog1.Title = "Save an Image File";
+			saveFileDialog1.Filter = "JPeg Image|*.jpg|Bitmap Image|*.bmp|PNG Image|*.png";
+
+			if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+			{
+				int width = Convert.ToInt32(viewPort.Width);
+				int height = Convert.ToInt32(viewPort.Height);
+				Bitmap bmp = new Bitmap(width, height);
+				viewPort.DrawToBitmap(bmp, new Rectangle(0, 0, width, height));
+				bmp.Save(saveFileDialog1.FileName, ImageFormat.Jpeg);
+			}
+			Close();
+		}
+		
+		/// <summary>
+		/// Събитието, което се прихваща, за да се превизуализира при изменение на модела.
+		/// </summary>
+		void ViewPortPaint(object sender, PaintEventArgs e)
+		{
+			dialogProcessor.ReDraw(sender, e);
+		}
+		
+		/// <summary>
+		/// Бутон, който поставя на произволно място правоъгълник със зададените размери.
+		/// Променя се лентата със състоянието и се инвалидира контрола, в който визуализираме.
+		/// </summary>
+		void DrawRectangleSpeedButtonClick(object sender, EventArgs e)
+		{
+			dialogProcessor.AddRandomRectangle();
+			
+			statusBar.Items[0].Text = "Последно действие: Рисуване на правоъгълник";
+			
+			viewPort.Invalidate();
+		}
+
+		/// <summary>
+		/// Прихващане на координатите при натискането на бутон на мишката и проверка (в обратен ред) дали не е
+		/// щракнато върху елемент. Ако е така то той се отбелязва като селектиран и започва процес на "влачене".
+		/// Промяна на статуса и инвалидиране на контрола, в който визуализираме.
+		/// Реализацията се диалогът с потребителя, при който се избира "най-горния" елемент от екрана.
+		/// </summary>
+		void ViewPortMouseDown(object sender, System.Windows.Forms.MouseEventArgs e)
+		{
+			if (pickUpSpeedButton.Checked) {
+				Shape sel = dialogProcessor.ContainsPoint(e.Location);
+				
+				if(sel != null)
+                {
+                    if (dialogProcessor.Selection.Contains(sel))
+						dialogProcessor.Selection.Remove(sel);
+
+                    else
+                     {
+							dialogProcessor.Selection.Add(sel);
+                     }
+                    
+                }
+					statusBar.Items[0].Text = "Последно действие: Селекция на примитив";
+					dialogProcessor.IsDragging = true;
+					dialogProcessor.LastLocation = e.Location;
+					viewPort.Invalidate();
+			}
+		}
+
+		/// <summary>
+		/// Прихващане на преместването на мишката.
+		/// Ако сме в режм на "влачене", то избрания елемент се транслира.
+		/// </summary>
+		void ViewPortMouseMove(object sender, System.Windows.Forms.MouseEventArgs e)
+		{
+			if (dialogProcessor.IsDragging) {
+				if (dialogProcessor.Selection != null) statusBar.Items[0].Text = "Последно действие: Влачене";
+				dialogProcessor.TranslateTo(e.Location);
+				viewPort.Invalidate();
+			}
+		}
+
+		/// <summary>
+		/// Прихващане на отпускането на бутона на мишката.
+		/// Излизаме от режим "влачене".
+		/// </summary>
+		void ViewPortMouseUp(object sender, System.Windows.Forms.MouseEventArgs e)
+		{
+			dialogProcessor.IsDragging = false;
+		}
+
+        private void SaveButton2(object sender, EventArgs e)
+        {
+
+			SaveFileDialog saveFileDialog1 = new SaveFileDialog();
+			saveFileDialog1.Filter = "JPeg Image|*.jpg|Bitmap Image|*.bmp|PNG Image|*.png";
+
+			if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+			{
+				int width = Convert.ToInt32(viewPort.Width);
+				int height = Convert.ToInt32(viewPort.Height);
+				Bitmap bmp = new Bitmap(width, height);
+				viewPort.DrawToBitmap(bmp, new Rectangle(0, 0, width, height));
+
+				bmp.Save(saveFileDialog1.FileName, ImageFormat.Jpeg);
+			}
+
+		}
+
+		private void DrawEllipseButtonClick(object sender, EventArgs e)
+        {
+			dialogProcessor.AddRandomEllipse();
+
+			statusBar.Items[0].Text = "Последно действие: Рисуване на елипса";
+
+			viewPort.Invalidate();
+		}
+
+        private void DrawLineButtonClick(object sender, EventArgs e)
+        {
+			dialogProcessor.AddRandomLine();
+
+			statusBar.Items[0].Text = "Последно действие: Рисуване на линия";
+
+			viewPort.Invalidate();
+		}
+
+		private void ColorToolButtonClick(object sender, EventArgs e)
+		{
+			if (colorDialog1.ShowDialog() == DialogResult.OK)
+			{
+				foreach (Shape item in dialogProcessor.ShapeList)
+					item.FillColor = colorDialog1.Color;
+
+				statusBar.Items[0].Text = "Последно действие: Смяна цвят";
+				viewPort.Invalidate();
+			}
+		}
+
+        private void toolStripButton6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void viewPort_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void openFromToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+			openFileDialog1 = new OpenFileDialog();
+
+		}
+
+        private void saveToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+			SaveFileDialog saveFileDialog1 = new SaveFileDialog();
+			saveFileDialog1.Filter = "JPeg Image|*.jpg|Bitmap Image|*.bmp|PNG Image|*.png";
+			if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+			{
+				int width = Convert.ToInt32(viewPort.Width);
+				int height = Convert.ToInt32(viewPort.Height);
+				Bitmap bmp = new Bitmap(width, height);
+				viewPort.DrawToBitmap(bmp, new Rectangle(0, 0, width, height));
+				bmp.Save(saveFileDialog1.FileName, ImageFormat.Jpeg);
+			}
+		}
+
+        private void toolStripButton6_Click_1(object sender, EventArgs e)
+        {
+			if (colorDialog2.ShowDialog() == DialogResult.OK)
+			{
+				foreach (Shape item in dialogProcessor.ShapeList)
+					item.StrokeColor = colorDialog2.Color;
+
+				viewPort.Invalidate();
+			}
+		}
+    }
+}
